3233f3aad199c84e1cd168ecbf71d301d23a0a8d
Marek Dulowski <dulowski.marek@gmail.com>
2019-05-24 14:03:43 +0200

    Refactor test to use Page class

--- src/test/kotlin/main/KotlinlangPageTest.kt
+++ src/test/kotlin/main/KotlinlangPageTest.kt
@@ -4,45 +4,45 @@
 package main
 
 import org.junit.After
-import kotlin.test.Test
 import org.junit.Before
+import kotlin.test.Test
 import kotlin.test.assertEquals
 
 import java.net.URL
 
-import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.RemoteWebDriver
 
 class KotlinlangPageTest {
-    private var driver: WebDriver
+    private lateinit var driver: WebDriver
     private lateinit var page: KotlinlangPage
 
-    init {
-        // That is a default port and url for Selenium Hub
-        val url = URL("http://localhost:4444/wd/hub")
-        // Instantiate RemoteWebDriver with ChromeOptions
-        driver = RemoteWebDriver(url, ChromeOptions())
-    }
-
+    // This happens before each test
     @Before
     fun setup() {
+        // That is a default port and url for Selenium Hub
+        val url = URL("http://localhost:4444/wd/hub")
+
+        // Instantiate RemoteWebDriver with ChromeOptions
+        driver = RemoteWebDriver(url, ChromeOptions())
+        // Get page instance
         page = KotlinlangPage(driver)
     }
 
-    
+    // This happens after each test
+    @After
+    fun teardown() {
+        // Close after each test
+        driver.close()
+    }
 
     @Test
-    fun testKotlinOrg() {
+    fun `display "Kotlin" in logo`() {
         // Visit kotlinlang.org website
-        driver.get("https://kotlinlang.org")
-        // Find element with "global-header-logo" class name
-        val element = driver.findElement(By.className("global-header-logo"))
+        page.open()
         // Test, if the found element's text is "Kotlin"
-        assertEquals(element.text, "Kotlin")
-        // Quit the driver to release resources
-        driver.quit()
+        assertEquals(page.getLogoText(), "Kotlin")
     }
 }
 
