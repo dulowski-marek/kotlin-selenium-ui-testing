af67140b7efc30a634ba9e897c5b8baaabe14b6c
Marek Dulowski <dulowski.marek@gmail.com>
2019-05-24 11:36:44 +0200

    Add kotlinlang.org test

--- src/test/kotlin/main/AppTest.kt
+++ src/test/kotlin/main/AppTest.kt
@@ -4,11 +4,34 @@
 package main
 
 import kotlin.test.Test
-import kotlin.test.assertNotNull
+import kotlin.test.assertEquals
+
+import java.net.URL
+
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.remote.RemoteWebDriver
 
 class AppTest {
-    @Test fun testAppHasAGreeting() {
-        val classUnderTest = App()
-        assertNotNull(classUnderTest.greeting, "app should have a greeting")
+    private var driver: WebDriver
+
+    init {
+        // That is a default port and url for Selenium Hub
+        val url = URL("http://localhost:4444/wd/hub")
+        // Instantiate RemoteWebDriver with ChromeOptions
+        driver = RemoteWebDriver(url, ChromeOptions())
+    }
+
+    @Test
+    fun testKotlinOrg() {
+        // Visit kotlinlang.org website
+        driver.get("https://kotlinlang.org")
+        // Find element with "global-header-logo" class name
+        val element = driver.findElement(By.className("global-header-logo"))
+        // Test, if the found element's text is "Kotlin"
+        assertEquals(element.text, "Kotlin")
+        // Quit the driver to release resources
+        driver.quit()
     }
 }
